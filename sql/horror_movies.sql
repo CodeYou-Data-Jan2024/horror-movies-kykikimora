144a71243483d94d066d12bfdfebef396572ac54
carol savchick <carolmariehinds@gmail.com>
2024-06-15 19:00:53 -0400

    added SQL

--- sql/horror_movies.sql
+++ sql/horror_movies.sql
@@ -1,1 +1,5 @@
--- Add your SQL here
+SELECT id AS 'Movie_ID', name AS 'Movie_Title', imbd_rating AS 'Rating'
+FROM movies
+WHERE genre = 'horror' and year <= 1985
+ORDER BY imbd_rating DESC
+Limit 3;
