161f1cb61e500cce1cbe15512500a9ce7e645746
Carol Savchick <carolsavchick@outlook.com>
2024-06-23 11:35:55 -0400

    finished sql assignment

--- sql/horror_movies.sql
+++ sql/horror_movies.sql
@@ -1,5 +1,5 @@
-SELECT id AS 'Movie_ID', name AS 'Movie_Title', imbd_rating AS 'Rating'
+SELECT id AS Movie_ID, name AS Movie_Title, imdb_rating AS Rating
 FROM movies
 WHERE genre = 'horror' and year <= 1985
-ORDER BY imbd_rating DESC
+ORDER BY imdb_rating DESC
 Limit 3;
